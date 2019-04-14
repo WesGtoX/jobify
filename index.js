7a0d0849bd7db24f7f7fd51258b3a2a595d77f25
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2019-04-14 02:40:27 -0300

    bloking admin from non localhost

--- index.js
+++ index.js
@@ -9,7 +9,17 @@ const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Prom
 
 const port = process.env.PORT || 3000
 
-app.set('viwes', path.join(__dirname, 'views'))
+// bloking admin from non localhost
+app.use('/admin', (req, res, next) => {
+    if(req.hostname === 'localhost') {
+        next()
+    } else {
+        res.send('Not allowed')
+    }
+})
+
+
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname,'public')))
 app.use(bodyParser.urlencoded({ extended: true }))
